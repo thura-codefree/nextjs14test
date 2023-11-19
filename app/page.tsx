aaddbee08fff31586078735153c507c7792e54c1
unknown <thurasoft@gmail.com>
2023-11-18 23:28:05 -0800

    dynamic dashboard error fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import localFont from "next/font/local";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Page({ status }: { status: string }) {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
