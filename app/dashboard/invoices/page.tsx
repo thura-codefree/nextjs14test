805071b67500c198fc400a0f6d443eba43a01089
unknown <thurasoft@gmail.com>
2023-12-31 09:38:41 -0800

    metadatas

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,7 +9,7 @@ import { fetchInvoicesPages } from "@/app/lib/data";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Invoice | Acme Dashboard",
+  title: "Invoice",
 };
 
 export default async function Page({
